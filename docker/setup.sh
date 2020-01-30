25485128ee1fc8e1194fa399f982b39f0efa9598
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-01-30 19:07:37 +0100

    Rename denoms in docker setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,10 +5,10 @@ PASSWORD=${PASSWORD:-1234567890}
 wasmd init --chain-id=testing testing
 (echo $PASSWORD; echo $PASSWORD) | wasmcli keys add validator
 # hardcode the validator account for this instance
-echo $PASSWORD | wasmd add-genesis-account validator 1000000000stake,1000000000cosm
+echo $PASSWORD | wasmd add-genesis-account validator 1000000000ustake,1000000000ucosm
 # (optionally) add a few more genesis accounts
 for addr in "$@"; do
-  wasmd add-genesis-account $addr 1000000000stake,1000000000cosm
+  wasmd add-genesis-account $addr 1000000000ustake,1000000000ucosm
 done
 # submit a genesis validator tx
 (echo $PASSWORD; echo $PASSWORD; echo $PASSWORD) | wasmd gentx --name validator
