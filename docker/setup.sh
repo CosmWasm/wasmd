f4683ce0f5a44627b3ba0370993c3ce2352dba6d
Simon Warta <simon@warta.it>
2020-02-09 10:49:15 +0100

    Do Double quote to prevent globbing and word splitting

--- docker/setup.sh
+++ docker/setup.sh
@@ -9,13 +9,13 @@ FEE=${2:-ucosm}
 wasmd init --chain-id=testing testing
 # staking/governance token is hardcoded in config, change this
 sed -i "s/\"stake\"/\"$STAKE\"/" "$HOME"/.wasmd/config/genesis.json
-(echo $PASSWORD; echo $PASSWORD) | wasmcli keys add validator
+(echo "$PASSWORD"; echo "$PASSWORD") | wasmcli keys add validator
 # hardcode the validator account for this instance
-echo $PASSWORD | wasmd add-genesis-account validator "1000000000$STAKE,1000000000$FEE"
+echo "$PASSWORD" | wasmd add-genesis-account validator "1000000000$STAKE,1000000000$FEE"
 # (optionally) add a few more genesis accounts
 for addr in "$@"; do
-  wasmd add-genesis-account $addr "1000000000$STAKE,1000000000$FEE"
+  wasmd add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE"
 done
 # submit a genesis validator tx
-(echo $PASSWORD; echo $PASSWORD; echo $PASSWORD) | wasmd gentx --name validator --amount "250000000$STAKE"
+(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmd gentx --name validator --amount "250000000$STAKE"
 wasmd collect-gentxs
