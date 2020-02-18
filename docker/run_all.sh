81e4b66919531cfb9270f8c181e26b18cecb9cae
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-02-18 22:37:33 +0100

    Minor adjustments to docker

--- docker/run_all.sh
+++ docker/run_all.sh
@@ -8,4 +8,4 @@ touch /root/log/wasmd.log
 sleep 4
 echo Starting Rest Server...
 
-wasmcli rest-server --laddr tcp://0.0.0.0:1317 --trust-node --cors
+./run_wasmcli.sh
---
+++ docker/run_rest_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -euo pipefail
+
+wasmcli rest-server --laddr tcp://0.0.0.0:1317 --trust-node --cors
