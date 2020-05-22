d61b2aafa01c2d33955f2c7461c8770960da7410
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-05-22 16:08:03 +0200

    Make Docker setup script more robust (for local testing)

--- docker/setup.sh
+++ docker/setup.sh
@@ -8,11 +8,14 @@ FEE=${FEE_TOKEN:-ucosm}
 wasmd init --chain-id=testing testing
 # staking/governance token is hardcoded in config, change this
 sed -i "s/\"stake\"/\"$STAKE\"/" "$HOME"/.wasmd/config/genesis.json
-(echo "$PASSWORD"; echo "$PASSWORD") | wasmcli keys add validator
+if ! wasmcli keys show validator; then
+  (echo "$PASSWORD"; echo "$PASSWORD") | wasmcli keys add validator
+fi
 # hardcode the validator account for this instance
 echo "$PASSWORD" | wasmd add-genesis-account validator "1000000000$STAKE,1000000000$FEE"
 # (optionally) add a few more genesis accounts
 for addr in "$@"; do
+  echo $addr
   wasmd add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE"
 done
 # submit a genesis validator tx
