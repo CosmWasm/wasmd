0ba1e68ff07f9c2be31152ba56f03efde9315030
Reuven Podmazo <reuven@enigma.co>
2020-06-04 15:44:37 +0300

    fixed test

--- x/wasm/internal/keeper/keeper_test.go
+++ x/wasm/internal/keeper/keeper_test.go
@@ -297,7 +297,7 @@ func TestExecute(t *testing.T) {
 
 	// make sure gas is properly deducted from ctx
 	gasAfter := ctx.GasMeter().GasConsumed()
-	require.Equal(t, uint64(0x7b38), gasAfter-gasBefore)
+	require.Equal(t, uint64(0x7fa1), gasAfter-gasBefore)
 
 	// ensure bob now exists and got both payments released
 	bobAcct = accKeeper.GetAccount(ctx, bob)
