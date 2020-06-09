a8b021905f92fd270dfe466bf55e348fc9ee2519
Assaf Morami <assaf.morami@gmail.com>
2020-06-09 16:18:38 +0300

    fix deps

--- x/wasm/handler.go
+++ x/wasm/handler.go
@@ -6,7 +6,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	cosmwasmtypes "github.com/enigmampc/EnigmaBlockchain/go-cosmwasm/types"
+	cosmwasmtypes "github.com/CosmWasm/go-cosmwasm/types"
 )
 
 const (
