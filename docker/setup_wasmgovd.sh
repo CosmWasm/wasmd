8fab88e594fc279d25d9f2d41a2e63e2bc0350fa
Alex Peters <alpe@users.noreply.github.com>
2020-07-15 09:00:29 +0200

    Introduce wasmgovd; disable wasm proposals with wasmd

---
+++ docker/setup_wasmgovd.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#set -o errexit -o nounset -o pipefail
+
+PASSWORD=${PASSWORD:-1234567890}
+STAKE=${STAKE_TOKEN:-ustake}
+FEE=${FEE_TOKEN:-ucosm}
+
+wasmgovd init --chain-id=testing testing
+# staking/governance token is hardcoded in config, change this
+sed -i "s/\"stake\"/\"$STAKE\"/" "$HOME"/.wasmgovd/config/genesis.json
+if ! wasmcli keys show validator; then
+  (echo "$PASSWORD"; echo "$PASSWORD") | wasmcli keys add validator
+fi
+# hardcode the validator account for this instance
+echo "$PASSWORD" | wasmgovd add-genesis-account validator "1000000000$STAKE,1000000000$FEE"
+# (optionally) add a few more genesis accounts
+for addr in "$@"; do
+  echo $addr
+  wasmgovd add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE"
+done
+# submit a genesis validator tx
+(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmgovd gentx --name validator --amount "250000000$STAKE"
+wasmgovd collect-gentxs
