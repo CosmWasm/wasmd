4951b2f7af8aa4275c3d9af1f277b1393b4cad08
Alex Peters <alpe@users.noreply.github.com>
2020-08-07 14:45:51 +0200

    Adjust docker scripts

--- docker/setup.sh
+++ docker/setup.sh
@@ -19,5 +19,5 @@ for addr in "$@"; do
   wasmd add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE"
 done
 # submit a genesis validator tx
-(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmd gentx --name validator --amount "250000000$STAKE"
+(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmd gentx validator --chain-id=testing --amount "250000000$STAKE"
 wasmd collect-gentxs
