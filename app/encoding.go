c06327aace4c7100443732be916f3c7bdbe851c5
Alex Peters <alpe@users.noreply.github.com>
2020-08-10 09:30:07 +0200

    Use our ModuleBasic var for encoding

--- app/encoding.go
+++ app/encoding.go
@@ -1,14 +1,15 @@
 package app
 
 import (
-	"github.com/CosmWasm/wasmd/x/wasm"
-	"github.com/cosmos/cosmos-sdk/simapp"
 	simparams "github.com/cosmos/cosmos-sdk/simapp/params"
+	"github.com/cosmos/cosmos-sdk/std"
 )
 
 func MakeEncodingConfig() simparams.EncodingConfig {
-	encodingConfig := simapp.MakeEncodingConfig() // todo: this is the simapp !!!
-	wasm.RegisterCodec(encodingConfig.Amino)
-	wasm.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	encodingConfig := simparams.MakeEncodingConfig() // todo: this is the simapp !!!
+	std.RegisterCodec(encodingConfig.Amino)
+	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	ModuleBasics.RegisterCodec(encodingConfig.Amino)
+	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	return encodingConfig
 }
