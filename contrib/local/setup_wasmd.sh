447fb07f6ee65de1cc4ee05ed2d07bad5c8673f4
Alexander Peters <alpe@users.noreply.github.com>
2021-01-06 10:05:36 +0100

    Upgrade to cosmos-sdk v0.40.0-rc6 (#354)
    * Upgrade to cosmos-sdk v0.40.0-rc6
    
    * Apply workaround for gentx cli issue
    
    * Update changelog

--- contrib/local/setup_wasmd.sh
+++ contrib/local/setup_wasmd.sh
@@ -22,5 +22,8 @@ for addr in "$@"; do
   wasmd add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE"
 done
 # submit a genesis validator tx
-(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmd gentx validator --chain-id="$CHAIN_ID" --amount "250000000$STAKE"
+## Workraround for https://github.com/cosmos/cosmos-sdk/issues/8251
+(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmd gentx validator "250000000$STAKE" --chain-id="$CHAIN_ID" --amount="250000000$STAKE"
+## should be:
+# (echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmd gentx validator "250000000$STAKE" --chain-id="$CHAIN_ID"
 wasmd collect-gentxs
