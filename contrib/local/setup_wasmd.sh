c30a5a35c565e295989c76582a0bed35a2b535c0
Piotr Husiatyński <phusiatynski@gmail.com>
2021-08-09 14:24:49 +0200

    Use shellcheck and shfmt on contrib scripts
    This commit does not introduce any functionality change. It is only
    applying suggestions from the shellcheck linter and reformats the code
    using shfmt.
    
    Shellcheck is a base shell linter that helps avoiding common shell
    script mistakes.
    Shfmt formats shell code similar to other Xfmt (i.e. gofmt) tools. No
    need to format code manually or argue about the style anymore!
    
    Both tools can be integrated with many editors or IDE.

--- contrib/local/setup_wasmd.sh
+++ contrib/local/setup_wasmd.sh
@@ -12,18 +12,25 @@ wasmd init --chain-id "$CHAIN_ID" "$MONIKER"
 ## OSX requires: -i.
 sed -i. "s/\"stake\"/\"$STAKE\"/" "$HOME"/.wasmd/config/genesis.json
 if ! wasmd keys show validator; then
-  (echo "$PASSWORD"; echo "$PASSWORD") | wasmd keys add validator
+  (
+    echo "$PASSWORD"
+    echo "$PASSWORD"
+  ) | wasmd keys add validator
 fi
 # hardcode the validator account for this instance
 echo "$PASSWORD" | wasmd add-genesis-account validator "1000000000$STAKE,1000000000$FEE"
 # (optionally) add a few more genesis accounts
 for addr in "$@"; do
-  echo $addr
+  echo "$addr"
   wasmd add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE"
 done
 # submit a genesis validator tx
 ## Workraround for https://github.com/cosmos/cosmos-sdk/issues/8251
-(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmd gentx validator "250000000$STAKE" --chain-id="$CHAIN_ID" --amount="250000000$STAKE"
+(
+  echo "$PASSWORD"
+  echo "$PASSWORD"
+  echo "$PASSWORD"
+) | wasmd gentx validator "250000000$STAKE" --chain-id="$CHAIN_ID" --amount="250000000$STAKE"
 ## should be:
 # (echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmd gentx validator "250000000$STAKE" --chain-id="$CHAIN_ID"
 wasmd collect-gentxs
