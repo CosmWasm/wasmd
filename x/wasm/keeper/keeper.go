73e599e859db38e3092ad013421f73d9d1b29020
Carlton Hanna <channa@figure.com>
2021-08-17 08:57:39 -0600

    Add truncation of addresses back, Link them to upstream issues that need resolving

--- x/wasm/keeper/keeper.go
+++ x/wasm/keeper/keeper.go
@@ -874,7 +874,9 @@ func BuildContractAddress(codeID, instanceID uint64) sdk.AccAddress {
 	contractID := make([]byte, 16)
 	binary.BigEndian.PutUint64(contractID[:8], codeID)
 	binary.BigEndian.PutUint64(contractID[8:], instanceID)
-	return sdkaddress.Module(types.ModuleName, contractID)
+	// TODO remove truncation when issue is resolved https://github.com/cosmos/ibc-go/issues/339
+	// related issue: https://github.com/cosmos/ibc/issues/594
+	return sdkaddress.Module(types.ModuleName, contractID)[:20]
 }
 
 // GetNextCodeID reads the next sequence id used for storing wasm code.
