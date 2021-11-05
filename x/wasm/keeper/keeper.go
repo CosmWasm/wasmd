445fbb7564cfe112eade3cad0767934cab08d7b3
Alex Peters <alpe@users.noreply.github.com>
2021-11-05 15:07:12 +0100

    Open read access to sequences

--- x/wasm/keeper/keeper.go
+++ x/wasm/keeper/keeper.go
@@ -911,18 +911,6 @@ func assertNil(err error) {
 	}
 }
 
-// GetNextCodeID reads the next sequence id used for storing wasm code.
-// Read only operation.
-func (k Keeper) GetNextCodeID(ctx sdk.Context) uint64 {
-	store := ctx.KVStore(k.storeKey)
-	bz := store.Get(types.KeyLastCodeID)
-	id := uint64(1)
-	if bz != nil {
-		id = binary.BigEndian.Uint64(bz)
-	}
-	return id
-}
-
 func (k Keeper) autoIncrementID(ctx sdk.Context, lastIDKey []byte) uint64 {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(lastIDKey)
@@ -935,8 +923,8 @@ func (k Keeper) autoIncrementID(ctx sdk.Context, lastIDKey []byte) uint64 {
 	return id
 }
 
-// peekAutoIncrementID reads the current value without incrementing it.
-func (k Keeper) peekAutoIncrementID(ctx sdk.Context, lastIDKey []byte) uint64 {
+// PeekAutoIncrementID reads the current value without incrementing it.
+func (k Keeper) PeekAutoIncrementID(ctx sdk.Context, lastIDKey []byte) uint64 {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(lastIDKey)
 	id := uint64(1)
