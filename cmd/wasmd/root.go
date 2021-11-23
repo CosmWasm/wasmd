15f5f3e93930adb6f33dc911903e3d9c06c6fdf2
Jacob Gadikian <jacobgadikian@gmail.com>
2021-11-23 17:43:39 +0700

    Small cleanup.
    Next, we will complete this by updating the ibc app and loosely base our changes on what provenance did.

--- cmd/wasmd/root.go
+++ cmd/wasmd/root.go
@@ -54,7 +54,7 @@ func NewRootCmd() (*cobra.Command, app.EncodingConfig) {
 	config.Seal()
 
 	initClientCtx := client.Context{}.
-		WithJSONMarshaler(clientcodec.NewProtoCodec(encodingConfig.Marshaler, encodingConfig.InterfaceRegistry)).
+		WithJSONCodec(clientcodec.NewProtoCodec(encodingConfig.Marshaler, encodingConfig.InterfaceRegistry)).
 		WithInterfaceRegistry(encodingConfig.InterfaceRegistry).
 		WithTxConfig(encodingConfig.TxConfig).
 		WithLegacyAmino(encodingConfig.Amino).
