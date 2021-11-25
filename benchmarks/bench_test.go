cb7497c53060ae618b61b0779f794b53bb7f692c
Alex Peters <alpe@users.noreply.github.com>
2021-11-25 10:17:04 +0100

    Fix flickering benchmark tests

--- benchmarks/bench_test.go
+++ benchmarks/bench_test.go
@@ -91,6 +91,7 @@ func BenchmarkTxSending(b *testing.B) {
 	for name, tc := range cases {
 		b.Run(name, func(b *testing.B) {
 			db := tc.db(b)
+			defer db.Close()
 			appInfo := InitializeWasmApp(b, db, tc.numAccounts)
 			txs := tc.txBuilder(b, &appInfo)
 
