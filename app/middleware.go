a3623f3a9b08ee28b1df4bf200e568553215a747
vuong <nguyenvuong1122000@gmail.com>
2022-02-23 13:43:42 +0700

    add CountTx Middleware

---
+++ app/middleware.go
@@ -0,0 +1,132 @@
+package app
+
+import (
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	"github.com/cosmos/cosmos-sdk/x/auth/middleware"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
+
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+
+)
+
+// ComposeMiddlewares compose multiple middlewares on top of a tx.Handler. The
+// middleware order in the variadic arguments is from outer to inner.
+//
+// Example: Given a base tx.Handler H, and two middlewares A and B, the
+// middleware stack:
+// ```
+// A.pre
+//   B.pre
+//     H
+//   B.post
+// A.post
+// ```
+// is created by calling `ComposeMiddlewares(H, A, B)`.
+func ComposeMiddlewares(txHandler tx.Handler, middlewares ...tx.Middleware) tx.Handler {
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		txHandler = middlewares[i](txHandler)
+	}
+
+	return txHandler
+}
+
+type TxHandlerOptions struct {
+	Debug bool
+
+	// TxDecoder is used to decode the raw tx bytes into a sdk.Tx.
+	TxDecoder sdk.TxDecoder
+
+	// IndexEvents defines the set of events in the form {eventType}.{attributeKey},
+	// which informs Tendermint what to index. If empty, all events will be indexed.
+	IndexEvents map[string]struct{}
+
+	LegacyRouter     sdk.Router
+	MsgServiceRouter *middleware.MsgServiceRouter
+
+	AccountKeeper     middleware.AccountKeeper
+	BankKeeper        types.BankKeeper
+	FeegrantKeeper    middleware.FeegrantKeeper
+	SignModeHandler   authsigning.SignModeHandler
+	SigGasConsumer    func(meter sdk.GasMeter, sig signing.SignatureV2, params types.Params) error
+	TXCounterStoreKey storetypes.StoreKey
+}
+}
+
+// NewDefaultTxHandler defines a TxHandler middleware stacks that should work
+// for most applications.
+func NewDefaultTxHandler(options TxHandlerOptions) (tx.Handler, error) {
+	if options.TxDecoder == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "txDecoder is required for middlewares")
+	}
+
+	if options.AccountKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "account keeper is required for middlewares")
+	}
+
+	if options.BankKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "bank keeper is required for middlewares")
+	}
+
+	if options.SignModeHandler == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "sign mode handler is required for middlewares")
+	}
+
+	var sigGasConsumer = options.SigGasConsumer
+	if sigGasConsumer == nil {
+		sigGasConsumer = DefaultSigVerificationGasConsumer
+	}
+
+	return ComposeMiddlewares(
+		middleware.NewRunMsgsTxHandler(options.MsgServiceRouter, options.LegacyRouter),
+		middleware.NewTxDecoderMiddleware(options.TxDecoder),
+		// Set a new GasMeter on sdk.Context.
+		//
+		// Make sure the Gas middleware is outside of all other middlewares
+		// that reads the GasMeter. In our case, the Recovery middleware reads
+		// the GasMeter to populate GasInfo.
+		middleware.GasTxMiddleware,
+		// Recover from panics. Panics outside of this middleware won't be
+		// caught, be careful!
+		middleware.RecoveryTxMiddleware,
+		// Choose which events to index in Tendermint. Make sure no events are
+		// emitted outside of this middleware.
+		middleware.NewIndexEventsTxMiddleware(options.IndexEvents),
+		// Reject all extension options which can optionally be included in the
+		// tx.
+		middleware.RejectExtensionOptionsMiddleware,
+		middleware.MempoolFeeMiddleware,
+		middleware.ValidateBasicMiddleware,
+		middleware.TxTimeoutHeightMiddleware,
+		middleware.ValidateMemoMiddleware(options.AccountKeeper),
+		middleware.ConsumeTxSizeGasMiddleware(options.AccountKeeper),
+		//Wasm Middleware
+		wasmkeeper.CountTxMiddleware(options.TXCounterStoreKey),
+
+		// No gas should be consumed in any middleware above in a "post" handler part. See
+		// ComposeMiddlewares godoc for details.
+		// `DeductFeeMiddleware` and `IncrementSequenceMiddleware` should be put outside of `WithBranchedStore` middleware,
+		// so their storage writes are not discarded when tx fails.
+		middleware.DeductFeeMiddleware(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper),
+		middleware.TxPriorityMiddleware,
+		middleware.SetPubKeyMiddleware(options.AccountKeeper),
+		middleware.ValidateSigCountMiddleware(options.AccountKeeper),
+		middleware.SigGasConsumeMiddleware(options.AccountKeeper, sigGasConsumer),
+		middleware.SigVerificationMiddleware(options.AccountKeeper, options.SignModeHandler),
+		middleware.IncrementSequenceMiddleware(options.AccountKeeper),
+		// Creates a new MultiStore branch, discards downstream writes if the downstream returns error.
+		// These kinds of middlewares should be put under this:
+		// - Could return error after messages executed succesfully.
+		// - Storage writes should be discarded together when tx failed.
+		middleware.WithBranchedStore,
+		// Consume block gas. All middlewares whose gas consumption after their `next` handler
+		// should be accounted for, should go below this middleware.
+		middleware.ConsumeBlockGasMiddleware,
+		middleware.NewTipMiddleware(options.BankKeeper),
+		//Ibc v3 middleware
+	), nil
+}
--- x/wasm/keeper/middleware.go
+++ x/wasm/keeper/middleware.go
@@ -9,29 +9,29 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/tx"
 )
 
-var _ tx.Handler = setCountTxHandler{}
+var _ tx.Handler = CountTxHandler{}
 
-type setCountTxHandler struct {
+type CountTxHandler struct {
 	storeKey storetypes.StoreKey
 	next     tx.Handler
 }
 
-// SetCountTxMiddleware sets CountTx in context
-func SetCountTxMiddleware(storeKey storetypes.StoreKey) tx.Middleware {
+// CountTxMiddleware sets CountTx in context
+func CountTxMiddleware(storeKey storetypes.StoreKey) tx.Middleware {
 	return func(txh tx.Handler) tx.Handler {
-		return setCountTxHandler{
+		return CountTxHandler{
 			storeKey: storeKey,
 			next:     txh,
 		}
 	}
 }
 
-func (sctm setCountTxHandler) setCountTxHandler(ctx context.Context, req tx.Request, simulate bool) error {
+func (ctm CountTxHandler) setCountTxHandler(ctx context.Context, req tx.Request, simulate bool) error {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
 	if simulate {
 		return nil
 	}
-	store := sdkCtx.KVStore(sctm.storeKey)
+	store := sdkCtx.KVStore(ctm.storeKey)
 	currentHeight := sdkCtx.BlockHeight()
 
 	var txCounter uint32 // start with 0
@@ -48,26 +48,26 @@ func (sctm setCountTxHandler) setCountTxHandler(ctx context.Context, req tx.Requ
 	return nil
 }
 
-func (sctm setCountTxHandler) CheckTx(ctx context.Context, req tx.Request, checkReq tx.RequestCheckTx) (tx.Response, tx.ResponseCheckTx, error) {
-	if err := sctm.setCountTxHandler(ctx, req, false); err != nil {
+func (ctm CountTxHandler) CheckTx(ctx context.Context, req tx.Request, checkReq tx.RequestCheckTx) (tx.Response, tx.ResponseCheckTx, error) {
+	if err := ctm.setCountTxHandler(ctx, req, false); err != nil {
 		return tx.Response{}, tx.ResponseCheckTx{}, err
 	}
 
-	return sctm.next.CheckTx(ctx, req, checkReq)
+	return ctm.next.CheckTx(ctx, req, checkReq)
 }
 
 // DeliverTx implements tx.Handler.DeliverTx.
-func (sctm setCountTxHandler) DeliverTx(ctx context.Context, req tx.Request) (tx.Response, error) {
-	if err := sctm.setCountTxHandler(ctx, req, false); err != nil {
+func (ctm CountTxHandler) DeliverTx(ctx context.Context, req tx.Request) (tx.Response, error) {
+	if err := ctm.setCountTxHandler(ctx, req, false); err != nil {
 		return tx.Response{}, err
 	}
-	return sctm.next.DeliverTx(ctx, req)
+	return ctm.next.DeliverTx(ctx, req)
 }
 
 // SimulateTx implements tx.Handler.SimulateTx.
-func (sctm setCountTxHandler) SimulateTx(ctx context.Context, req tx.Request) (tx.Response, error) {
-	if err := sctm.setCountTxHandler(ctx, req, true); err != nil {
+func (ctm CountTxHandler) SimulateTx(ctx context.Context, req tx.Request) (tx.Response, error) {
+	if err := ctm.setCountTxHandler(ctx, req, true); err != nil {
 		return tx.Response{}, err
 	}
-	return sctm.next.SimulateTx(ctx, req)
+	return ctm.next.SimulateTx(ctx, req)
 }
