3bf50b16ce69f10fd5f35086a90af05d7881e150
romelukaku <gundamaster5@gmail.com>
2022-02-24 10:17:34 +0700

    rm rest handler from gov proposal handler

--- x/wasm/client/proposal_handler.go
+++ x/wasm/client/proposal_handler.go
@@ -4,18 +4,17 @@ import (
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
 
 	"github.com/CosmWasm/wasmd/x/wasm/client/cli"
-	"github.com/CosmWasm/wasmd/x/wasm/client/rest"
 )
 
 // ProposalHandlers define the wasm cli proposal types and rest handler.
 var ProposalHandlers = []govclient.ProposalHandler{
-	govclient.NewProposalHandler(cli.ProposalStoreCodeCmd, rest.StoreCodeProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalInstantiateContractCmd, rest.InstantiateProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalMigrateContractCmd, rest.MigrateProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalExecuteContractCmd, rest.ExecuteProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalSudoContractCmd, rest.SudoProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalUpdateContractAdminCmd, rest.UpdateContractAdminProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalClearContractAdminCmd, rest.ClearContractAdminProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalPinCodesCmd, rest.PinCodeProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalUnpinCodesCmd, rest.UnpinCodeProposalHandler),
+	govclient.NewProposalHandler(cli.ProposalStoreCodeCmd),
+	govclient.NewProposalHandler(cli.ProposalInstantiateContractCmd),
+	govclient.NewProposalHandler(cli.ProposalMigrateContractCmd),
+	govclient.NewProposalHandler(cli.ProposalExecuteContractCmd),
+	govclient.NewProposalHandler(cli.ProposalSudoContractCmd),
+	govclient.NewProposalHandler(cli.ProposalUpdateContractAdminCmd),
+	govclient.NewProposalHandler(cli.ProposalClearContractAdminCmd),
+	govclient.NewProposalHandler(cli.ProposalPinCodesCmd),
+	govclient.NewProposalHandler(cli.ProposalUnpinCodesCmd),
 }
