1748b226f54a33642da317d693ac5075605a9dc7
mattverse <mattpark1028@gmail.com>
2022-09-01 18:25:00 +0900

    Fix lint

--- x/wasm/keeper/stargate_bindings.go
+++ x/wasm/keeper/stargate_bindings.go
@@ -1,12 +1,10 @@
 package keeper
 
-//DONTCOVER
-
 import (
 	"sync"
 )
 
-// StargateLayerBindings keeps whitelist and its deterministic
+// StargateWhitelist keeps whitelist and its deterministic
 // response binding for stargate queries.
 //
 // The query can be multi-thread, so we have to use
