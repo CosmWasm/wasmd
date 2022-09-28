8adfdb324a4a9d054671a99aab30a85a478ed2c7
Jacob Gadikian <jacobgadikian@gmail.com>
2022-09-29 02:34:57 +0700

    update gas per suggestion

--- x/wasm/keeper/keeper_test.go
+++ x/wasm/keeper/keeper_test.go
@@ -400,7 +400,7 @@ func TestInstantiate(t *testing.T) {
 
 	gasAfter := ctx.GasMeter().GasConsumed()
 	if types.EnableGasVerification {
-		require.Equal(t, uint64(0x1964f), gasAfter-gasBefore)
+		require.Equal(t, uint64(0x1a455), gasAfter-gasBefore)
 	}
 
 	// ensure it is stored properly
