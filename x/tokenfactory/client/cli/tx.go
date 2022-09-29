0266340426f156645cc27689aad98f2f54d399d7
GNaD <cpt.meoz@gmail.com>
2022-09-29 15:43:34 +0700

    add cli tx

--- x/tokenfactory/client/cli/tx.go
+++ x/tokenfactory/client/cli/tx.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
 )
 
@@ -21,6 +22,9 @@ func GetTxCmd() *cobra.Command {
 
 	cmd.AddCommand(
 		CreateDenomCmd(),
+		MintCmd(),
+		BurnCmd(),
+		ChangeAdminCmd(),
 	)
 
 	return cmd
@@ -44,6 +48,95 @@ func CreateDenomCmd() *cobra.Command {
 			msg := types.NewMsgCreateDenom(
 				clientCtx.GetFromAddress().String(),
 				subdenom,
+			)
+
+			return tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func MintCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "mint [amount] [flags]",
+		Short: "Mint a denom to admin address. Must have admin authority to do so.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			txf := tx.NewFactoryCLI(clientCtx, cmd.Flags()).WithTxConfig(clientCtx.TxConfig).WithAccountRetriever(clientCtx.AccountRetriever)
+
+			amount, err := sdk.ParseCoinNormalized(args[0])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgMint(
+				clientCtx.GetFromAddress().String(),
+				amount,
+			)
+
+			return tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func BurnCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "burn [amount] [flags]",
+		Short: "Burn tokens from an admin address. Must have admin authority to do so.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			txf := tx.NewFactoryCLI(clientCtx, cmd.Flags()).WithTxConfig(clientCtx.TxConfig).WithAccountRetriever(clientCtx.AccountRetriever)
+
+			amount, err := sdk.ParseCoinNormalized(args[0])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgBurn(
+				clientCtx.GetFromAddress().String(),
+				amount,
+			)
+
+			return tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func ChangeAdminCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "change-admin [denom] [new-admin-address] [flags]",
+		Short: "Changes the admin address for a factory-created denom. Must have admin authority to do so.",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			txf := tx.NewFactoryCLI(clientCtx, cmd.Flags()).WithTxConfig(clientCtx.TxConfig).WithAccountRetriever(clientCtx.AccountRetriever)
+
+			msg := types.NewMsgChangeAdmin(
+				clientCtx.GetFromAddress().String(),
+				args[0],
+				args[1],
 			)
 
 			return tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msg)
