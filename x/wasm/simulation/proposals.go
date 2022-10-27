cee5615741a95a0206ed7eaecdd30460ff7eab90
GNaD <cpt.meoz@gmail.com>
2022-10-27 15:08:45 +0700

    add proposal for store contract

--- x/wasm/simulation/proposals.go
+++ x/wasm/simulation/proposals.go
@@ -1,14 +1,44 @@
 package simulation
 
 import (
-	"github.com/CosmWasm/wasmd/x/wasm/keeper"
+	"math/rand"
+
+	"github.com/CosmWasm/wasmd/app/params"
+	"github.com/CosmWasm/wasmd/x/wasm/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
 )
 
-func ProposalContents(k keeper.Keeper) []simtypes.WeightedProposalContent {
+const (
+	OpWeightStoreCodeProposal = "op_weight_store_code_proposal"
+)
 
+func ProposalContents(wasmKeeper WasmKeeper) []simtypes.WeightedProposalContent {
+	return []simtypes.WeightedProposalContent{
+		simulation.NewWeightedProposalContent(
+			OpWeightStoreCodeProposal,
+			params.DefaultWeightStoreCodeProposal,
+			SimulatetoreCodeProposal(wasmKeeper),
+		),
+	}
 }
 
-func SimulatetoreCodeProposal(k keeper.Keeper) simtypes.ContentSimulatorFn {
+func SimulatetoreCodeProposal(wasmKeeper WasmKeeper) simtypes.ContentSimulatorFn {
+	return func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) simtypes.Content {
+		simAccount, _ := simtypes.RandomAcc(r, accs)
 
+		wasmBz := []byte("fdsagfds")
+
+		permission := wasmKeeper.GetParams(ctx).InstantiateDefaultPermission.With(simAccount.Address)
+
+		return types.NewStoreCodeProposal(
+			simtypes.RandStringOfLength(r, 10),
+			simtypes.RandStringOfLength(r, 10),
+			simAccount.Address.String(),
+			wasmBz,
+			&permission,
+			false,
+		)
+	}
 }
