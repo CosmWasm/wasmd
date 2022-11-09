4fe2fbc8f322efdaf187e2e5c99ce32fd1df06f0
Alexander Peters <alpe@users.noreply.github.com>
2022-11-09 08:58:05 +0100

    Deprecate REST endpoints (#1070)
    * Deprecate REST support
    
    * Add deprecation and nolint annotations

--- x/wasm/module.go
+++ x/wasm/module.go
@@ -24,7 +24,7 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	"github.com/CosmWasm/wasmd/x/wasm/client/cli"
-	"github.com/CosmWasm/wasmd/x/wasm/client/rest"
+	"github.com/CosmWasm/wasmd/x/wasm/client/rest" //nolint:staticcheck
 	"github.com/CosmWasm/wasmd/x/wasm/keeper"
 	"github.com/CosmWasm/wasmd/x/wasm/simulation"
 	"github.com/CosmWasm/wasmd/x/wasm/types"
