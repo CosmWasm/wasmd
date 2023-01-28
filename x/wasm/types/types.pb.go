8918e284b2885bfb3dfe90c68339b42debd32b5d
Alex Peters <alpe@users.noreply.github.com>
2023-01-28 22:34:41 +0100

    Move helper method out of generated file

--- x/wasm/types/types.pb.go
+++ x/wasm/types/types.pb.go
@@ -1569,17 +1569,6 @@ func (m *AccessConfig) Unmarshal(dAtA []byte) error {
 	return nil
 }
 
-// si
-func (a AccessConfig) AllAuthorizedAddresses() []string {
-	switch a.Permission {
-	case AccessTypeAnyOfAddresses:
-		return a.Addresses
-	case AccessTypeOnlyAddress:
-		return []string{a.Address}
-	}
-	return []string{}
-}
-
 func (m *Params) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
