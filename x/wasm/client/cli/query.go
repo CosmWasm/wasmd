2ccffed778f69bf935fbf3a7e3a47405140fa4d8
Nikhil Suri <nikhilsuri@comcast.net>
2023-02-13 19:13:58 +0000

    list-contract-by-code bugfix

--- x/wasm/client/cli/query.go
+++ x/wasm/client/cli/query.go
@@ -580,6 +580,7 @@ func GetCmdListContractsByCreator() *cobra.Command {
 		SilenceUsage: true,
 	}
 	flags.AddQueryFlagsToCmd(cmd)
+	flags.AddPaginationFlagsToCmd(cmd, "list contracts by creator")
 	return cmd
 }
 
