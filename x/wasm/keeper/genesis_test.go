2f01f8d9afaa1684d390b11af098b02abd4bc2d6
Jacob Gadikian <jacobgadikian@gmail.com>
2023-02-15 03:27:37 +0700

    make proto-gen

--- x/wasm/keeper/genesis_test.go
+++ x/wasm/keeper/genesis_test.go
@@ -687,7 +687,7 @@ func setupKeeper(t *testing.T) (*Keeper, sdk.Context, []storetypes.StoreKey) {
 }
 
 // isValidFuzzStateModels: check if the keysModel is contain the key of model
-func isValidFuzzStateModels(t *testing.T, keysModel []string, stateModels []types.Model ) ([]string, bool){
+func isValidFuzzStateModels(t *testing.T, keysModel []string, stateModels []types.Model) ([]string, bool) {
 	tmpKeysModel := keysModel
 	isContainKey := false
 	for _, model := range stateModels {
