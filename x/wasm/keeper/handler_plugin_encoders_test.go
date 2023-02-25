120b7b1d87b3cec71a80d5f069d2d949b2127ab6
Jacob Gadikian <jacobgadikian@gmail.com>
2023-02-25 07:18:32 +0700

    indent-error-flow

--- x/wasm/keeper/handler_plugin_encoders_test.go
+++ x/wasm/keeper/handler_plugin_encoders_test.go
@@ -543,10 +543,10 @@ func TestEncoding(t *testing.T) {
 			if tc.expError {
 				assert.Error(t, err)
 				return
-			} else {
-				require.NoError(t, err)
-				assert.Equal(t, tc.output, res)
 			}
+			require.NoError(t, err)
+			assert.Equal(t, tc.output, res)
+
 			// and valid sdk message
 			for _, v := range res {
 				gotErr := v.ValidateBasic()
