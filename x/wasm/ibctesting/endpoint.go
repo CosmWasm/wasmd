340a93d37978b615a8be1923113d4bfe65fc73de
Alex Peters <alpe@users.noreply.github.com>
2023-03-16 11:13:18 +0100

    Minor updates

--- x/wasm/ibctesting/endpoint.go
+++ x/wasm/ibctesting/endpoint.go
@@ -2,9 +2,9 @@ package ibctesting
 
 import (
 	"fmt"
-	"github.com/cosmos/cosmos-sdk/baseapp"
 	"strings"
 
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	clienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
 	connectiontypes "github.com/cosmos/ibc-go/v7/modules/core/03-connection/types"
