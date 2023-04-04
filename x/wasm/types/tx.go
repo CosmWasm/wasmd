a249a6d6de9e8b9aa44d135bdc5c7aba8a599f9a
Pino' Surace <pino.surace@live.it>
2023-04-04 14:47:28 +0200

    fix conflicts

--- x/wasm/types/tx.go
+++ x/wasm/types/tx.go
@@ -261,11 +261,7 @@ func (msg MsgUpdateAdmin) ValidateBasic() error {
 		return sdkerrors.Wrap(err, "new admin")
 	}
 	if strings.EqualFold(msg.Sender, msg.NewAdmin) {
-<<<<<<< HEAD
-		return sdkerrors.Wrap(ErrInvalidMsg, "new admin is the same as the old")
-=======
-		return errorsmod.Wrap(ErrInvalid, "new admin is the same as the old")
->>>>>>> 4c906d5a (fix : removed the unnecessary usage of  ErrInvalidMsg  (#1311))
+		return sdkerrors.Wrap(ErrInvalid, "new admin is the same as the old")
 	}
 	return nil
 }
