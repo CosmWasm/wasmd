435a9f731399d02bd184449c38f580da533233f7
Pino' Surace <pino.surace@live.it>
2023-05-23 16:35:39 +0200

    Add more tests + fix bug in cli

--- tests/system/basic_test.go
+++ tests/system/basic_test.go
@@ -48,7 +48,27 @@ func TestBasicWasmTest(t *testing.T) {
 
 	t.Log("Instantiate wasm code")
 	initMsg := fmt.Sprintf(`{"verifier":%q, "beneficiary":%q}`, randomBech32Addr(), randomBech32Addr())
-	newContractAddr := cli.WasmInstantiate(codeID, initMsg)
+	newContractAddr := cli.WasmInstantiate(codeID, initMsg, "--admin="+defaultSrcAddr, "--label=label1", "--from="+defaultSrcAddr)
 	assert.Equal(t, expContractAddr, newContractAddr)
 	assert.Len(t, done(), 1)
+
+	t.Log("Update Instantiate Config")
+	qResult = cli.CustomQuery("q", "wasm", "code-info", fmt.Sprint(codeID))
+	assert.Equal(t, "Everybody", gjson.Get(qResult, "instantiate_permission.permission").String())
+
+	rsp := cli.CustomCommand("tx", "wasm", "update-instantiate-config", fmt.Sprint(codeID), "--instantiate-anyof-addresses="+cli.GetKeyAddr(defaultSrcAddr), "--from="+defaultSrcAddr)
+	RequireTxSuccess(t, rsp)
+
+	qResult = cli.CustomQuery("q", "wasm", "code-info", fmt.Sprint(codeID))
+	assert.Equal(t, "AnyOfAddresses", gjson.Get(qResult, "instantiate_permission.permission").String())
+	assert.Equal(t, cli.GetKeyAddr(defaultSrcAddr), gjson.Get(qResult, "instantiate_permission.addresses.#").Array()[0].String())
+
+	t.Log("Set contract admin")
+	newAdmin := randomBech32Addr()
+	rsp = cli.CustomCommand("tx", "wasm", "set-contract-admin", newContractAddr, newAdmin, "--from="+defaultSrcAddr)
+	RequireTxSuccess(t, rsp)
+
+	qResult = cli.CustomQuery("q", "wasm", "contract", newContractAddr)
+	actualAdmin := gjson.Get(qResult, "contract_info.admin").String()
+	assert.Equal(t, newAdmin, actualAdmin)
 }
