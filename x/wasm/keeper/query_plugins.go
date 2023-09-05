36fbddd4d72c6f573690f6b54c0d72834bb761f5
Pino' Surace <pino.surace@live.it>
2023-09-05 17:03:48 +0200

    Add logging for query err before redacting

--- x/wasm/keeper/query_plugins.go
+++ x/wasm/keeper/query_plugins.go
@@ -71,6 +71,7 @@ func (q QueryHandler) Query(request wasmvmtypes.QueryRequest, gasLimit uint64) (
 	}
 
 	// Issue #759 - we don't return error string for worries of non-determinism
+	moduleLogger(q.Ctx).Info("Redacting submessage error", "cause", err)
 	return nil, redactError(err)
 }
 
