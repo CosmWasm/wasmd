cb1fc1bb18fba129dc35ee88622dd7aa4c28dd1d
Alex Peters <alpe@users.noreply.github.com>
2023-10-09 14:40:21 +0200

    Update

--- tests/system/upgrade_test.go
+++ tests/system/upgrade_test.go
@@ -29,7 +29,7 @@ func TestChainUpgrade(t *testing.T) {
 	currentBranchBinary := sut.ExecBinary
 	sut.ExecBinary = legacyBinary
 	sut.SetupChain()
-	votingPeriod := 10 * time.Second // enough time to vote
+	votingPeriod := 5 * time.Second // enough time to vote
 	sut.ModifyGenesisJSON(t, SetGovVotingPeriod(t, votingPeriod))
 
 	const (
@@ -44,6 +44,7 @@ func TestChainUpgrade(t *testing.T) {
 	// set some state to ensure that migrations work
 	verifierAddr := cli.AddKey("verifier")
 	beneficiary := randomBech32Addr()
+	cli.FundAddress(verifierAddr, "1000stake")
 
 	t.Log("Launch hackatom contract")
 	codeID := cli.WasmStore("./testdata/hackatom.wasm.gzip")
@@ -89,13 +90,12 @@ func TestChainUpgrade(t *testing.T) {
 	sut.ExecBinary = currentBranchBinary
 	sut.StartChain(t)
 
-	t.Skip("wasmvm 1.4 upgrade fails, currently. Skipping for now")
 	// ensure that state matches expectations
 	gotRsp = cli.QuerySmart(contractAddr, `{"verifier":{}}`)
 	require.Equal(t, fmt.Sprintf(`{"data":{"verifier":"%s"}}`, verifierAddr), gotRsp)
 	// and contract execution works as expected
-	RequireTxSuccess(t, cli.WasmExecute(contractAddr, verifierAddr, `{"release":{}}`))
-	assert.Equal(t, 1_000_000, cli.QueryBalance(beneficiary, "stake"))
+	RequireTxSuccess(t, cli.WasmExecute(contractAddr, `{"release":{}}`, verifierAddr))
+	assert.Equal(t, int64(1_000_000), cli.QueryBalance(beneficiary, "stake"))
 }
 
 const cacheDir = "binaries"
