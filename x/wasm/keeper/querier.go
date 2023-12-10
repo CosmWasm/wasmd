a59a586196e5a688d64e2e1480262efd3020ded4
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-12-09 20:48:45 -0700

    implement BuildAddress grpc query

--- x/wasm/keeper/querier.go
+++ x/wasm/keeper/querier.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 	"encoding/binary"
+	"encoding/hex"
 	"runtime/debug"
 
 	"google.golang.org/grpc/codes"
@@ -405,4 +406,37 @@ func ensurePaginationParams(req *query.PageRequest) (*query.PageRequest, error) 
 		req.Limit = maxResultEntries
 	}
 	return req, nil
+}
+
+func (q GrpcQuerier) BuildAddress(c context.Context, req *types.QueryBuildAddressRequest) (*types.QueryBuildAddressResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	codeHash, err := hex.DecodeString(req.CodeHash)
+	if err != nil {
+		return nil, err
+	}
+	creator, err := sdk.AccAddressFromBech32(req.CreatorAddress)
+	if err != nil {
+		return nil, err
+	}
+	salt, err := hex.DecodeString(req.Salt)
+	if err != nil {
+		return nil, err
+	}
+	if len(salt) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "empty salt")
+	}
+	if req.InitArgs == nil {
+		return &types.QueryBuildAddressResponse{
+			Address: BuildContractAddressPredictable(codeHash, creator, salt, []byte{}).String(),
+		}, nil
+	}
+	initMsg := types.RawContractMessage(req.InitArgs)
+	if err := initMsg.ValidateBasic(); err != nil {
+		return nil, err
+	}
+	return &types.QueryBuildAddressResponse{
+		Address: BuildContractAddressPredictable(codeHash, creator, salt, initMsg).String(),
+	}, nil
 }
