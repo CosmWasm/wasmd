315bf5ca1868bee0bde249da5865752fb098ab0e
Simon Warta <simon@warta.it>
2024-01-26 17:18:28 +0100

    Generalize "pinned" to "discount" for cases where contract is in memory

--- x/wasm/keeper/wasmtesting/gas_register.go
+++ x/wasm/keeper/wasmtesting/gas_register.go
@@ -9,8 +9,8 @@ import (
 // MockGasRegister mock that implements keeper.GasRegister
 type MockGasRegister struct {
 	CompileCostFn       func(byteLength int) storetypes.Gas
-	SetupContractCostFn func(pinned bool, msgLen int) storetypes.Gas
-	ReplyCostFn         func(pinned bool, reply wasmvmtypes.Reply) storetypes.Gas
+	SetupContractCostFn func(discount bool, msgLen int) storetypes.Gas
+	ReplyCostFn         func(discount bool, reply wasmvmtypes.Reply) storetypes.Gas
 	EventCostsFn        func(evts []wasmvmtypes.EventAttribute) storetypes.Gas
 	ToWasmVMGasFn       func(source storetypes.Gas) uint64
 	FromWasmVMGasFn     func(source uint64) storetypes.Gas
@@ -31,18 +31,18 @@ func (m MockGasRegister) UncompressCosts(byteLength int) storetypes.Gas {
 	return m.UncompressCostsFn(byteLength)
 }
 
-func (m MockGasRegister) SetupContractCost(pinned bool, msgLen int) storetypes.Gas {
+func (m MockGasRegister) SetupContractCost(discount bool, msgLen int) storetypes.Gas {
 	if m.SetupContractCostFn == nil {
 		panic("not expected to be called")
 	}
-	return m.SetupContractCostFn(pinned, msgLen)
+	return m.SetupContractCostFn(discount, msgLen)
 }
 
-func (m MockGasRegister) ReplyCosts(pinned bool, reply wasmvmtypes.Reply) storetypes.Gas {
+func (m MockGasRegister) ReplyCosts(discount bool, reply wasmvmtypes.Reply) storetypes.Gas {
 	if m.ReplyCostFn == nil {
 		panic("not expected to be called")
 	}
-	return m.ReplyCostFn(pinned, reply)
+	return m.ReplyCostFn(discount, reply)
 }
 
 func (m MockGasRegister) EventCosts(evts []wasmvmtypes.EventAttribute, _ wasmvmtypes.Events) storetypes.Gas {
