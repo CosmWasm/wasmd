7bcdadc7395256f11b3fd94d920d0efb026cf4b5
lesterli <fulfilself@gmail.com>
2024-06-15 18:35:18 -0400

    fix: comment typo
    This PR fixes the comment typo of the function `ExecuteContractCmd`.

--- x/wasm/client/cli/tx.go
+++ x/wasm/client/cli/tx.go
@@ -358,7 +358,7 @@ func parseInstantiateArgs(rawCodeID, initMsg string, kr keyring.Keyring, sender 
 	return &msg, msg.ValidateBasic()
 }
 
-// ExecuteContractCmd will instantiate a contract from previously uploaded code.
+// ExecuteContractCmd will execute a contract method using its address and JSON-encoded arguments.
 func ExecuteContractCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "execute [contract_addr_bech32] [json_encoded_send_args] --amount [coins,optional]",
