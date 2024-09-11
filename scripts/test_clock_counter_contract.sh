d4bdd304d446b14021caa0aec2cd6e8b7fc16102
ducphamle2 <ducphamle212@gmail.com>
2024-09-10 22:04:47 -0700

    fix: clock param proposal to new gov style, fix endblock wrong func sig

--- scripts/test_clock_counter_contract.sh
+++ scripts/test_clock_counter_contract.sh
@@ -8,6 +8,7 @@ set -ux
 WASM_PATH=${WASM_PATH:-"$PWD/scripts/wasm_file/cw-clock-example.wasm"}
 ARGS="--chain-id testing -y --keyring-backend test --gas auto --gas-adjustment 1.5 -b sync"
 VALIDATOR1_ARGS=${VALIDATOR1_ARGS:-"--from validator1 --home $HOME/.oraid/validator1"}
+VALIDATOR2_ARGS=${VALIDATOR2_ARGS:-"--from validator2 --home $HOME/.oraid/validator2"}
 QUERY_MSG=${QUERY_MSG:-'{"get_config":{}}'}
 
 CONTRACT_GAS_LIMIT=${CONTRACT_GAS_LIMIT:-"123000000"}
@@ -15,54 +16,70 @@ TITLE=${TITLE:-"add contract to clock module"}
 INITIAL_DEPOSIT=${INITIAL_DEPOSIT:-"10000000orai"}
 DESCRIPTION=${DESCRIPTION:-"add cw-clock contract to clock module"}
 HIDE_LOGS="/dev/null"
+CLOCK_PROPOSAL_FILE=${CLOCK_PROPOSAL_FILE:-"$PWD/scripts/json/clock-proposal.json"}
 
 store_ret=$(oraid tx wasm store $WASM_PATH $VALIDATOR1_ARGS $ARGS --output json)
 store_txhash=$(echo $store_ret | jq -r '.txhash')
 # need to sleep 1s for tx already in block
-sleep 1
-store_info=$(oraid query tx $store_txhash --output json)
-code_id=$(echo $store_info | jq -r '.events[4].attributes[] | select(.key | contains("code_id")).value')
+sleep 2
+# need to use temp.json since there's a weird error: jq: parse error: Invalid string: control characters from U+0000 through U+001F must be escaped at line 1, column 72291
+# probably because of weird characters from the raw code bytes
+oraid query tx $store_txhash --output json > temp.json
+code_id=$(cat temp.json | jq -r '.events[4].attributes[] | select(.key | contains("code_id")).value')
+rm temp.json
 oraid tx wasm instantiate $code_id '{}' --label 'cw clock contract' $VALIDATOR1_ARGS --admin $(oraid keys show validator1 --keyring-backend test --home $HOME/.oraid/validator1 -a) $ARGS > $HIDE_LOGS
 # need to sleep 1s for tx already in block
 sleep 1
 contract_address=$(oraid query wasm list-contract-by-code $code_id --output json | jq -r '.contracts | last')
 echo "cw-clock contract address: $contract_address, $CONTRACT_GAS_LIMIT, $TITLE, $INITIAL_DEPOSIT, $DESCRIPTION"
 
-oraid tx clock add-contract $contract_address $CONTRACT_GAS_LIMIT "$TITLE" "$INITIAL_DEPOSIT" "$DESCRIPTION" $VALIDATOR1_ARGS $ARGS --output json
-# add_contract_result=$(oraid tx clock add-contract $contract_address $CONTRACT_GAS_LIMIT "$TITLE" "$INITIAL_DEPOSIT" "$DESCRIPTION" $VALIDATOR1_ARGS $ARGS --output json)
-# proposal_id=$(echo $add_contract_result | jq -r '.logs[0].events[4].attributes[] | select(.key | contains("proposal_id")).value')
-# echo "proposal id: $proposal_id"
+# create clock proposal to whitelist contract
+update_proposal() {
+    cat $CLOCK_PROPOSAL_FILE | jq "$1" >$PWD/scripts/json/temp_proposal.json && mv $PWD/scripts/json/temp_proposal.json $CLOCK_PROPOSAL_FILE
+}
 
-# oraid tx gov vote $proposal_id yes --from validator1 --home "$HOME/.oraid/validator1" $ARGS > $HIDE_LOGS && oraid tx gov vote $proposal_id yes --from validator2 --home "$HOME/.oraid/validator2" $ARGS > $HIDE_LOGS
+# update authority proposal.json
+MODULE_ACCOUNT=$(oraid query auth module-account gov --output json | jq '.account.value.address')
+update_proposal ".messages[0][\"authority\"]=$MODULE_ACCOUNT"
+# update clock contract in the proposal
+update_proposal ".messages[0].params.contract_addresses=[\"$contract_address\""]
 
-# # sleep to wait til the proposal passes
-# echo "Sleep til the proposal passes..."
-# sleep 5
+store_ret=$(oraid tx gov submit-proposal $CLOCK_PROPOSAL_FILE $VALIDATOR1_ARGS $ARGS --output json)
+store_txhash=$(echo $store_ret | jq -r '.txhash')
+# sleep 2s before vote to wait for tx confirm
+sleep 2
+proposal_id=$(oraid query tx $store_txhash --output json | jq -r '.events[4].attributes[] | select(.key | contains("proposal_id")).value')
 
-# # Query the counter
-# counter_before=$(oraid query wasm contract-state smart $contract_address $QUERY_MSG --node "tcp://localhost:26657" --output json | jq -r '.data.val | tonumber')
-# sleep 2
-# echo "cw-clock counter_before: $counter_before"
+oraid tx gov vote $proposal_id yes $VALIDATOR1_ARGS $ARGS > $HIDE_LOGS && oraid tx gov vote $proposal_id yes $VALIDATOR2_ARGS $ARGS > $HIDE_LOGS
 
-# counter_after=$(oraid query wasm contract-state smart $contract_address $QUERY_MSG --node "tcp://localhost:26657" --output json | jq -r '.data.val | tonumber')
-# sleep 2
-# echo "cw-clock counter_after: $counter_after"
+# sleep to wait til the proposal passes
+echo "Sleep til the proposal passes..."
+sleep 5
 
-# if [[ $counter_after == $counter_before ]]; then
-#   echo "Clock Counter Test Failed"; exit 1
-# fi
+# Query the counter
+counter_before=$(oraid query wasm contract-state smart $contract_address $QUERY_MSG --node "tcp://localhost:26657" --output json | jq -r '.data.val | tonumber')
+sleep 2
+echo "cw-clock counter_before: $counter_before"
 
-# QUERY_MSG='{"get_after_sudo":{}}'
-# after_sudo_before=$(oraid query wasm contract-state smart $contract_address $QUERY_MSG --node "tcp://localhost:26657" --output json | jq -r '.data | tonumber')
-# sleep 2
-# echo "cw-clock after sudo before: $after_sudo_before"
+counter_after=$(oraid query wasm contract-state smart $contract_address $QUERY_MSG --node "tcp://localhost:26657" --output json | jq -r '.data.val | tonumber')
+sleep 2
+echo "cw-clock counter_after: $counter_after"
 
-# after_sudo_after=$(oraid query wasm contract-state smart $contract_address $QUERY_MSG --node "tcp://localhost:26657" --output json | jq -r '.data | tonumber')
-# sleep 2
-# echo "cw-clock after sudo after: $after_sudo_after"
+if [[ $counter_after == $counter_before ]]; then
+  echo "Clock Counter Test Failed"; exit 1
+fi
 
-# if [[ $after_sudo_before == $after_sudo_after ]]; then
-#   echo "Clock Counter After Sudo Test Failed"; exit 1
-# fi
+QUERY_MSG='{"get_after_sudo":{}}'
+after_sudo_before=$(oraid query wasm contract-state smart $contract_address $QUERY_MSG --node "tcp://localhost:26657" --output json | jq -r '.data | tonumber')
+sleep 2
+echo "cw-clock after sudo before: $after_sudo_before"
 
-# echo "Clock Counter Test Passed"
+after_sudo_after=$(oraid query wasm contract-state smart $contract_address $QUERY_MSG --node "tcp://localhost:26657" --output json | jq -r '.data | tonumber')
+sleep 2
+echo "cw-clock after sudo after: $after_sudo_after"
+
+if [[ $after_sudo_before == $after_sudo_after ]]; then
+  echo "Clock Counter After Sudo Test Failed"; exit 1
+fi
+
+echo "Clock Counter Test Passed"
