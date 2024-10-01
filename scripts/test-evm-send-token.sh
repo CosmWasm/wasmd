e45edcc01ac99a5d9ff7c48f041f993f2ba19ca3
ledanghuy1811 <huygamo567@gmail.com>
2024-10-01 14:12:02 +0700

    feat: bump to latest ethermint module and remove fee collector from blocked address

--- scripts/test-evm-send-token.sh
+++ scripts/test-evm-send-token.sh
@@ -9,7 +9,7 @@ PRIVATE_KEY_ETH=${PRIVATE_KEY_ETH:-"021646C7F742C743E60CC460C56242738A3951667E71
 # run evm send token test
 current_dir=$PWD
 
-clone or pull latest repo
+# clone or pull latest repo
 if [ -d "$PWD/../evm-send-token" ]; then
     cd ../evm-send-token
     git pull origin master
