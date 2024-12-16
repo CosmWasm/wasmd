aeced6c1578ef3afa39a0306b408b1d284e6ce9a
Christoph Otter <chris@confio.gmbh>
2024-12-16 11:07:17 +0100

    Add comment about cosmwasm_1_4 capability

--- app/wasm.go
+++ app/wasm.go
@@ -4,6 +4,10 @@ package app
 // See https://github.com/CosmWasm/cosmwasm/blob/main/docs/CAPABILITIES-BUILT-IN.md
 // This functionality is going to be moved upstream: https://github.com/CosmWasm/wasmvm/issues/425
 func AllCapabilities() []string {
+	// This branch contains the 0.33.x version line. It was updated from wasmvm v1.3.0 to v1.5.x
+	// to be able to provide security updates.
+	// Therefore this deliberately does not include "cosmwasm_1_4" to not enable the features
+	// guarded by that capability, even though it is theoretically available.
 	return []string{
 		"iterator",
 		"staking",
