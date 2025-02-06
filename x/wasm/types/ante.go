d036a349dc829b0434503bf64fc85cd05526edd9
josefkedwards <cedwards19606389@yahoo.com>
2025-02-06 00:17:36 -0500

    Update ante.go

--- x/wasm/types/ante.go
+++ x/wasm/types/ante.go
@@ -134,3 +134,32 @@ func (d TxContractsDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bo
 	txContracts := types.NewTxContracts()
 	return next(types.WithTxContracts(ctx, txContracts), tx, simulate)
 }
+
+func (a CountTXDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+	if simulate {
+		return next(ctx, tx, simulate)
+	}
+	store := a.storeService.OpenKVStore(ctx)
+	currentHeight := ctx.BlockHeight()
+
+	var txCounter uint32 // start with 0
+	// load counter when exists
+	bz, err := store.Get(types.TXCounterPrefix)
+	if err != nil {
+		return ctx, errorsmod.Wrap(err, "read tx counter")
+	}
+	if bz != nil {
+		lastHeight, val := decodeHeightCounter(bz)
+		if currentHeight == lastHeight {
+			// then use stored counter
+			txCounter = val
+		} // else use `0` from above to start with
+	}
+	// store next counter value for current height using atomic increment
+	newTxCounter := txCounter + 1
+	err = store.Set(types.TXCounterPrefix, encodeHeightCounter(currentHeight, newTxCounter))
+	if err != nil {
+		return ctx, errorsmod.Wrap(err, "store tx counter")
+	}
+	return next(types.WithTXCounter(ctx, newTxCounter), tx, simulate)
+}
