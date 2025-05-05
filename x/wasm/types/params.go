9ba963cd902fcb835663628c40c0c6628043ca59
Tronica <wudmytrotest404@gmail.com>
2025-05-05 15:26:59 +0200

    Update params.go

--- x/wasm/types/params.go
+++ x/wasm/types/params.go
@@ -12,7 +12,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-var AllAccessTypes []AccessType = []AccessType{
+var AllAccessTypes = []AccessType{
 	AccessTypeNobody,
 	AccessTypeAnyOfAddresses,
 	AccessTypeEverybody,
