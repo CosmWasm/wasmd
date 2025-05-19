23895b4f734752daf20317b147332b07b9d0e527
Tomasz Kulik <tomek.kulik2@gmail.com>
2025-05-19 15:58:34 +0200

    chore: Fix tests

--- tests/wasmibctesting/utils.go
+++ tests/wasmibctesting/utils.go
@@ -822,7 +822,9 @@ func ParseAckFromEventsV2(events []abci.Event) ([]byte, error) {
 		if ev.Type == channeltypes.EventTypeWriteAck {
 			for _, attr := range ev.Attributes {
 				if attr.Key == channeltypesv2.AttributeKeyEncodedAckHex {
-					bz, err := hex.DecodeString(attr.Value)
+
+					// The first two bytes is a noise
+					bz, err := hex.DecodeString(attr.Value[4:])
 					if err != nil {
 						panic(err)
 					}
